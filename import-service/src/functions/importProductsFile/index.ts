8273499e575acda8399f638d15efe01a2c2cb0f7
Raj Mameday <raj_mameday@epam.com>
2023-05-14 15:56:06 +0300

    Task 7. Add auth service

--- import-service/src/functions/importProductsFile/index.ts
+++ import-service/src/functions/importProductsFile/index.ts
@@ -7,6 +7,8 @@ export default {
       http: {
         method: 'get',
         path: 'import',
+        authorizer: 'arn:aws:lambda:us-east-1:649764365395:function:authorization-service-dev-basicAuthorizer',
+        cors: true,
       },
     },
   ],
